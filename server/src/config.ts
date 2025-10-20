31532d5c2f354866a2cc592ca66a7d2eb1d77293
Luis Jesus <ljcm47@gmail.com>
2025-10-19 20:26:22 -0400

    test

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://192.168.1.110:4000"; // si pruebas en dispositivo físico, usa tu IP local
+//export const API_URL = "http://192.168.1.104:4000"; // si pruebas en dispositivo físico, usa tu IP local
+export const API_URL = "http://192.168.232.27:4000"; // si pruebas en dispositivo físico, usa tu IP local
